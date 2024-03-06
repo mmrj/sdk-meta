4e6940c6f2b06e4249603fabd09f76708e3caed8
Casey Waldren <cwaldren@launchdarkly.com>
2024-03-06 15:03:31 -0800

    chore: add github validation workflows

---
+++ scripts/ci/json-schemas.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+mapfile -t schemas < <(git grep -Fl 'https://json-schema.org' '**/*.json')
+
+function runTest() {
+    primary=$(realpath --relative-to="$(pwd)" "$1")
+
+    # Build up a list of reference schemas to use, being careful to ignore the
+    # schema under test. If we don't skip that one, ajv will generate an error
+    # about duplicate $ids.
+    ajvFlags=()
+    for schema in "${schemas[@]}"; do
+        if [[ "$schema" != "$primary" ]]; then
+            ajvFlags+=(-r "${schema}")
+        fi
+    done
+
+    npx --package=ajv-cli --package=ajv-formats ajv validate --spec=draft2020 -s "$primary" "${ajvFlags[@]}" -d "$2"
+}
+
+runTest ./schemas/features.json ./data/features.json
+runTest ./schemas/types.json ./data/types.json
---
+++ scripts/ci/sdk-consistency.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Checks that for each SDK ID defined in sdks.json, all other files have an entry for that ID.
+# If not, outputs a diff containing the missing IDs and exits with a non-zero status.
+
+cleanup() {
+  rm ./data/*.json.keys
+}
+
+trap cleanup EXIT
+
+jq -r 'values[]' ./data/sdks.json | sort > ./data/sdks.json.keys
+
+for file in ./data/*.json; do
+  # Skip sdks.json itself
+  if [ "$file" == "./data/sdks.json" ]; then
+    continue
+  fi
+  jq -r 'keys[]' "$file" | sort > "$file.keys"
+  if diff -q sdks.json.keys "$file.keys" > /dev/null; then
+    echo "$file is consistent"
+  else
+    echo "$file is missing some SDK IDs:"
+    diff ./data/sdks.json.keys "$file.keys"
+    exit 1
+  fi
+done
