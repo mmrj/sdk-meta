bfd2b453ab3b32571cf461226cb1bc76192a13fe
Casey Waldren <cwaldren@launchdarkly.com>
2024-03-15 12:33:14 -0700

    fix: change names product to be string value instead of array

--- scripts/generate-products.sh
+++ scripts/generate-products.sh
@@ -6,7 +6,7 @@ sqlite3 -json metadata.sqlite3 "SELECT * from sdk_languages;" |
 jq 'reduce .[] as $item ({}; .[$item.id] += [$item.language])' > products/languages.json
 
 sqlite3 -json metadata.sqlite3 "SELECT * from sdk_names;" |
-  jq 'reduce .[] as $item ({}; .[$item.id] += [$item.name])' > products/names.json
+  jq 'reduce .[] as $item ({}; .[$item.id] = $item.name)' > products/names.json
 
 sqlite3 -json metadata.sqlite3 "SELECT * from sdk_types;" |
   jq 'reduce .[] as $item ({}; .[$item.id] = $item.type)' > products/types.json
