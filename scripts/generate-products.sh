5ebfddd4ee6b6710320164d2795667d1aab2808c
Casey Waldren <cwaldren@launchdarkly.com>
2024-03-15 16:59:43 -0700

    chore: make sure json products are sorted (#18)

--- scripts/generate-products.sh
+++ scripts/generate-products.sh
@@ -2,17 +2,21 @@
 
 set -e
 
+# Notes:
+# -S argument to JQ is used to sort the keys of the output objects so we get more deterministic output,
+# and it's easier to compare diffs between commits to the repo.
+
 sqlite3 -json metadata.sqlite3 "SELECT * from sdk_languages;" |
-jq 'reduce .[] as $item ({}; .[$item.id] += [$item.language])' > products/languages.json
+  jq -S 'reduce .[] as $item ({}; .[$item.id] += [$item.language])' > products/languages.json
 
 sqlite3 -json metadata.sqlite3 "SELECT * from sdk_names;" |
-  jq 'reduce .[] as $item ({}; .[$item.id] = $item.name)' > products/names.json
+  jq -S 'reduce .[] as $item ({}; .[$item.id] = $item.name)' > products/names.json
 
 sqlite3 -json metadata.sqlite3 "SELECT * from sdk_types;" |
-  jq 'reduce .[] as $item ({}; .[$item.id] = $item.type)' > products/types.json
+  jq -S 'reduce .[] as $item ({}; .[$item.id] = $item.type)' > products/types.json
 
 sqlite3 -json metadata.sqlite3 "SELECT * from sdk_repos;" |
-  jq 'reduce .[] as $item ({}; .[$item.id] += {github: $item.github})' > products/repos.json
+  jq -S 'reduce .[] as $item ({}; .[$item.id] += {github: $item.github})' > products/repos.json
 
 sqlite3 -json metadata.sqlite3 "SELECT * from sdk_features;" |
-  jq 'reduce .[] as $item ({}; .[$item.id] = {($item.feature): {introduced: $item.introduced, deprecated: $item.deprecated, removed: $item.removed}})' > products/features.json
+  jq -S 'reduce .[] as $item ({}; .[$item.id] = {($item.feature): {introduced: $item.introduced, deprecated: $item.deprecated, removed: $item.removed}})' > products/features.json
