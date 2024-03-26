5eb2bc2bf577b5e962b47888ab7856cf19f4ff0b
Casey Waldren <cwaldren@launchdarkly.com>
2024-03-25 17:12:08 -0700

    chore: add cpp-sdks (#23)

--- scripts/ci/format-json.sh
+++ scripts/ci/format-json.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+jq . config.json > config.json.tmp && mv config.json.tmp config.json
+
 for file in ./products/*.json; do
   jq . "$file" > "$file.tmp" && mv "$file.tmp" "$file"
 done
