68def5bdad45c6c05dc6935f2ca2a3e07d7ca596
Molly <molly.jones@launchdarkly.com>
2024-05-03 15:44:09 -0700

    add feature descriptions (draft)

--- schemas/sdk_metadata.sql
+++ schemas/sdk_metadata.sql
@@ -64,8 +64,29 @@ INSERT INTO sdk_type_info (type, description) VALUES
                                               ('edge', 'Primarily used to delivery flag payloads to edge services.');
 
 INSERT INTO sdk_feature_info (feature, description) VALUES
-    ('u2c', 'The concept of Users is replaced with Contexts, which can be used to represent users, devices, or other entities.'),
-    ('hooks', 'Hooks are collections of user-defined callbacks that are executed by the SDK at various points of interest, usually for metrics or tracing.');
+    ('Application metadata', 'Specify application and application version information.'),
+    ('Automatic environment attributes', 'Automatically include device and application data in each evaluated context.'),
+    ('Big segments', 'Configure a persistent store to hold segments that are either synced from external tools, or that contain an arbitrarily large number of contexts of any one context kind.'),
+    ('Bootstrapping', 'Provide an initial set of flag values that are immediately available during client initialization.'),
+    ('Contexts and context kinds', 'Evaluate flags based on contexts. A context is a generalized way of referring to the people, services, machines, or other resources that encounter feature flags. SDKs without this feature only support flag evaluation for users.'),
+    ('Experimentation', 'Connect a flag with one or more metrics to measure end-user behavior for different variations of a flag. Requires minimum SDK versions, but no SDK configuration.'),
+    ('Flag evaluation reasons', 'Receive information about how a flag variation was calculated, for example, because it matched a specific targeting rule.'),
+    ('Getting all flags', 'Return the flag variations for all feature flags for a given context.'),
+    ('Hooks', 'Define callbacks that are executed by the SDK at various points of interest, usually for metrics or tracing.'),
+    ('Migration flags', 'Configure how to read and write data for an old and new system, determine which stage of a migration the application is in, execute the correct read and write calls for each stage.'),
+    ('Multiple environments', 'Evaluate flags from multiple environments using a single client instance'),
+    ('Offline mode', 'Close the SDK\'s connection to LaunchDarkly. Use cached or fallback values for each flag evaluation.'),
+    ('OpenTelemetry', 'Add flag evaluation information to OpenTelemetry spans.'),
+    ('Private attributes', 'Use context attribute values for targeting, but do not send them to LaunchDarkly.'),
+    ('Reading flags from a file', 'Use flag values, specified in JSON or YAML files, for all flag evaluations. Useful for testing or prototyping; do not use in production.'),
+    ('Relay Proxy in daemon mode', 'Configure the SDK to connect to the Relay Proxy\'s data store.'),
+    ('Relay Proxy in proxy mode', 'Configure the SDK to connect to a load balancer set up in front of several Relay Proxy instances.'),
+    ('Secure mode', 'For clent-side SDKs, require a hash, signed with the SDK key for the LaunchDarkly environment, to evaluate flag variations. For server-side or edge SDKs, generate a secure mode hash.'),
+    ('Sending custom events', 'Record actions taken in your application as events. You can connect to these events to metrics for use in experiments.'),
+    ('Storing data', 'Configure an external database as a feature store. Persist flag data across application restarts.'),
+    ('Subscribing to flag changes', 'Use a listener pattern to subscribe to flag change notifications.'),
+    ('Test data sources', 'Mock the behavior of the SDK. Useful for unit tests; cannot be used in production.'),
+    ('Web proxy configuration', 'Configure the SDK to connect to LaunchDarkly through a web proxy.');
 
 INSERT INTO sdk_language_info (language) VALUES
     ('Apex'),
